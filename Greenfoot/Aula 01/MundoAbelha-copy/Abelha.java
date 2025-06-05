618c0165e3a281be808d21365edfba5ae7502dca
Glenda Renata Silva <glendarenata2014@gmail.com>
2025-06-04 21:53:07 -0300

    Conteudo Greenfoot

--- Greenfoot/Aula 01/MundoAbelha-copy/Abelha.java
+++ Greenfoot/Aula 01/MundoAbelha-copy/Abelha.java
@@ -1,10 +1,10 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Abelha here.
+ * Esta é uma classe que representará uma Abelha
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Glenda Renata Silva
+ * @version 2025-06-04
  */
 public class Abelha extends Actor
 {
@@ -16,5 +16,11 @@ public class Abelha extends Actor
     {
         // Add your action code here.
         move(1);
+        if (Greenfoot.isKeyDown("left")){
+            turn(-5);
+        }
+        if (Greenfoot.isKeyDown("right")){
+           turn(5); 
+        }
     }
 }
--- Greenfoot/Aula 01/MundoAbelha-copy/BeeWorld.java
+++ Greenfoot/Aula 01/MundoAbelha-copy/BeeWorld.java
@@ -1,19 +1,19 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class MyWorld here.
+ * Definição da Classe BeeWorld.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Glenda Renata Silva
+ * @version 2025-06-04
  */
-public class MyWorld extends World
+public class BeeWorld extends World
 {
 
     /**
-     * Constructor for objects of class MyWorld.
+     * Constructor for objects of class BeeWorld.
      * 
      */
-    public MyWorld()
+    public BeeWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(800, 600, 1); 
@@ -27,27 +27,37 @@ public class MyWorld extends World
     private void prepare()
     {
         Aranha aranha = new Aranha();
-        addObject(aranha,30,35);
+        addObject(aranha,30,35); 
+        
         Abelha abelha = new Abelha();
         addObject(abelha,34,199);
+        
         Mosca mosca = new Mosca();
         addObject(mosca,30,337);
+        
         Mosca mosca2 = new Mosca();
         addObject(mosca2,80,364);
         mosca2.setLocation(83,305);
+        
         Mosca mosca3 = new Mosca();
         addObject(mosca3,83,305);
         mosca2.setLocation(85,307);
+        
         Mosca mosca4 = new Mosca();
         addObject(mosca4,85,307);
+        
         Mosca mosca5 = new Mosca();
         addObject(mosca5,82,357);
+        
         Mosca mosca6 = new Mosca();
         addObject(mosca6,148,373);
+        
         Mosca mosca7 = new Mosca();
         addObject(mosca7,146,330);
+        
         Mosca mosca8 = new Mosca();
         addObject(mosca8,159,292);
+        
         Mosca mosca9 = new Mosca();
         addObject(mosca9,212,330);
         mosca2.setLocation(85,305);
